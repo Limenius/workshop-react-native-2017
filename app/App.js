0210b2974be4fb76fa0267cd527e6b232de598cf
Nacho Martín <nacho@limenius.com>
2017-09-15 13:56:50 +0200

    Elements playground

--- app/App.js
+++ app/App.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react'
 import {
-  Text,
   View,
-  StyleSheet
+  ActivityIndicator,
+  Button,
+  Picker,
+  Slider,
+  StyleSheet,
+  Switch,
+  TextInput,
+  Image,
 } from 'react-native'
+import { movies } from './data'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hi there!</Text>
+      <View>
+        {/*
+          <Text>Hi there!</Text>
+        */}
+        {/*<Button
+          onPress={() => alert('Hi')}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="This is a sample button"
+        />*/}
+        {/* Image from static file
+        <Image style={{height: 100, width: 100}} source={{uri: 'https://lorempixel.com/100/100/cats/'}}/>
+        */}
+        {/* Image from uri
+        <Image style={{height: 100, width: 100}} source={require('./images/pulp-fiction.jpg')}/>*/}
+        {/*<ActivityIndicator/>*/}
+        {/*<Picker
+          onValueChange={(itemValue, itemIndex) => alert(`You picked ${itemValue}`)}
+        >
+          { movies.map(movie => (
+            <Picker.Item key={movie.name} label={movie.name} value={movie.name} />
+          ))}
+        </Picker>*/}
+        {/*<Slider/>*/}
+        {/*<Switch/>*/}
+        {/*<TextInput
+          defaultValue="Type here"
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        />*/}
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-})
