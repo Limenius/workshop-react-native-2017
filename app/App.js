c54d104d407edd61ab9ca45f02a72505cba8ade2
Nacho Martín <nacho@limenius.com>
2017-09-17 18:33:25 +0200

    Flex intro

--- app/App.js
+++ app/App.js
@@ -5,25 +5,38 @@ import {
   StyleSheet
 } from 'react-native'
 
+const Colors = {
+    highlight: '#E23E57',
+    accent: '#65799B',
+    subtleAccent: '#E2EFF1',
+    backgroundSection: '#E2EFF1',
+    text: '#555273',
+    background: '#FFFFFF',
+}
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hi there!</Text>
+        <Box style={{backgroundColor: Colors.subtleAccent, flex: 0.5}}/>
+        <Box style={{backgroundColor: Colors.accent, flex: 0.2}}/>
+        <Box style={{backgroundColor:Colors.text, flex: 0.3}}/>
       </View>
     )
   }
 }
 
+const Box = ({style, text}) => <View style={{ backgroundColor: '#E23E57', padding: 9,  ...style}}><Text style={{color: 'white'}}>{text}</Text></View>
+
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
+    //alignItems: 'center',
+    //alignItems: 'stretch',
+    //alignItems: 'baseline',
+    //alignItems: 'flex-start',
+    //alignItems: 'flex-end',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    backgroundColor: '#E2EFF1',
   },
 })
