e13371354110550cf4d8642b946878d04b206076
Nacho Martín <nacho@limenius.com>
2017-09-21 15:33:32 +0200

    Example of interactivity

--- app/components/MovieList.js
+++ app/components/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import MainHeader from './MainHeader'
 import ListItem from './ListItem'
+import SearchBox from './SearchBox'
 import Colors from '../styles/colors'
 import { baseUrl } from '../constants'
 
@@ -9,6 +10,7 @@ import {
   ActivityIndicator,
   TouchableHighlight,
   FlatList,
+  TextInput,
 } from 'react-native'
 
 export default class MovieList extends Component {
@@ -20,30 +22,41 @@ export default class MovieList extends Component {
     super(props)
     this.state = {
       movies: [],
+      moviesList: [],
       isLoading: true,
+      searchText: '',
     }
+
+    //this.setSearchText = this.setSearchText.bind(this)
+
   }
 
   componentDidMount() {
     return fetch(baseUrl + '/movies')
       .then((response) => response.json())
       .then((responseJson) => {
-        // Simulate server latency
-        //setTimeout(() =>
-          this.setState({
-            isLoading: false,
-            movies: responseJson,
-          })
-        //  , 5000)
+        this.setState({
+          isLoading: false,
+          movies: responseJson,
+          moviesList: responseJson,
+        })
       })
       .catch((error) => {
         console.error(error);
       })
   }
 
+  //setSearchText(text) {
+  //  const regex = new RegExp(text, "gi")
+  //  this.setState({
+  //    searchText: text,
+  //    moviesList: this.state.movies.filter(movie => movie.name.match(regex)),
+  //  })
+  //}
+
   render() {
     const { navigate } = this.props.navigation
-    const { isLoading, movies } = this.state
+    const { isLoading, } = this.state
 
     if (isLoading) {
       return (
@@ -53,9 +66,14 @@ export default class MovieList extends Component {
       )
     }
     return (
-      <View>
+      <View style={{flex: 1}}>
+        {/*<SearchBox
+          value={this.state.searchText}
+          onChangeText={this.setSearchText}
+        />*/}
         <FlatList
-          data={movies}
+          style={{flex: 1}}
+          data={this.state.movies/*List*/}
           ListHeaderComponent={<MainHeader/>}
           keyExtractor={item => item.name}
           renderItem={({item}) =>
---
+++ app/components/SearchBox.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Colors from '../styles/colors'
+import FontSizes from '../styles/fontSizes'
+
+import {
+  StyleSheet,
+  View,
+  TextInput,
+} from 'react-native'
+
+const SearchBox = ({value, onChangeText}) => (
+  <View style={Styles.container}>
+    <TextInput
+      style={Styles.input}
+      value={value.searchText}
+      onChangeText={onChangeText}
+    />
+  </View>
+)
+
+const Styles = StyleSheet.create({
+  container: {
+    padding: 9,
+    paddingLeft: 18,
+    paddingRight: 18,
+    backgroundColor: Colors.backgroundSection,
+  },
+  input: {
+    borderColor: Colors.accent,
+    backgroundColor: Colors.background,
+    borderRadius: 9,
+    borderWidth: 1,
+    paddingHorizontal: 9,
+    paddingVertical: 4,
+    color: Colors.text,
+    fontSize: FontSizes.footnote,
+  }
+})
+
+export default SearchBox
